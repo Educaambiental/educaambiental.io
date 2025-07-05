2aeb6e0c22cccc0d425c458959358f22069b98b7
Educaambiental <projeducaambiental@gmail.com>
2025-07-05 13:32:06 -0400

    Add files via upload
    Restaurando após excluir para atualizar

---
+++ script.js
@@ -0,0 +1,38 @@
+if ('scrollRestoration' in history) {
+        history.scrollRestoration = 'manual';
+    }
+
+    window.addEventListener('beforeunload', () => {
+        localStorage.setItem('scrollPos', window.scrollY);
+    });
+
+    window.addEventListener('load', () => {
+        const scrollPos = localStorage.getItem('scrollPos');
+        if (scrollPos) {
+            window.scrollTo(0, scrollPos);
+        }
+    });
+
+document.addEventListener("DOMContentLoaded", function() {
+    let tamanhoBase = 16;
+    let tamanhoAtual = tamanhoBase;
+
+    function aplicarTamanho() {
+        document.body.style.fontSize = tamanhoAtual + "px";
+    }
+
+    document.getElementById("aumentar").addEventListener("click", function() {
+        tamanhoAtual += 2;
+        aplicarTamanho();
+    });
+
+    document.getElementById("diminuir").addEventListener("click", function() {
+        tamanhoAtual -= 2;
+        aplicarTamanho();
+    });
+
+    document.getElementById("resetar").addEventListener("click", function() {
+        tamanhoAtual = tamanhoBase; 
+        aplicarTamanho();
+    });
+});
